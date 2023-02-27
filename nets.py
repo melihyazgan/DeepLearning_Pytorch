073c2c75f291253abbd83faf382a4ef0b2188886
melihyazgan <yazganmelih@outlook.com>
2023-02-27 16:52:39 +0100

    InceptionModule implemented

--- nets.py
+++ nets.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 class CNN(nn.Module):
     def __init__(self):
         super(CNN,self).__init__()
@@ -18,3 +19,26 @@ class CNN(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+class InceptionModule(nn.Module):
+    def __init__(self,in_channels,out_channels):
+        super(InceptionModule,self).__init__()
+        self.branch1=nn.Sequential(nn.Conv2d(in_channels,out_channels=out_channels,kernel_size=1,stride=1,padding=0),nn.ReLU())
+        conv3_1 = nn.Conv2d(in_channels,out_channels=out_channels,kernel_size=1,stride=1,padding=0)
+        conv3_3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.branch2 = nn.Sequential(conv3_1, conv3_3,nn.ReLU())
+
+        conv5_1 = nn.Conv2d(in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        conv5_5 = nn.Conv2d(out_channels, out_channels, kernel_size=5, stride=1, padding=2)
+        self.branch3 = nn.Sequential(conv5_1,conv5_5,nn.ReLU())
+
+        max_pool_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        conv_max_1 = nn.Conv2d(in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        self.branch4 = nn.Sequential(max_pool_1, conv_max_1,nn.ReLU())
+
+    def forward(self, input):
+        output1 = self.branch1(input)
+        output2 = self.branch2(input)
+        output3 = self.branch3(input)
+        output4 = self.branch4(input)
+        return torch.cat([output1, output2, output3, output4], dim=1)
